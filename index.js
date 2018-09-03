862906bbf357c3dadb65285de96a2db82e14d462
Mandi Cai <mandi@biobright.org>
2018-09-02 22:48:45 -0400

    Initial commit with visuals (bubble force chart and bubble line chart)

---
+++ index.js
@@ -0,0 +1,102 @@
+// TAKEAWAYS
+// - You cannot append text (or really anything) to circles directly. You should wrap
+// the circle into a group and append the text to the group
+// Use the x and y of the circle to set the x and y of the text, not dx or dy.
+// Use text-anchor: middle to center the text.
+// The function ticked controls how the circles and text change position during
+// the force simulation.
+
+let width = 600, height = 600
+
+let svg = d3.select('#maternity-bubble-chart')
+  .append('svg')
+  .attr('viewBox', '0 0' + ' ' + width + ' ' + height)
+
+let industries = ['Technology','Philanthropy','Hospitality','Government: Federal','Educational Services']
+
+let color = d3.scaleSequential(d3.interpolatePuRd).domain([0, industries.length + 2])
+
+d3.csv('maternity-data.csv').then(data => {
+  let nodes = data
+
+  // BUBBLE FORCE CREATION
+  let simulation = d3.forceSimulation(data) // creates simulation
+    .force('charge', d3.forceManyBody().strength(10)) // applies attraction or repelling force
+    .force('center', d3.forceCenter(width / 2, height / 2)) // pulls points towards a center
+    .force('collision', d3.forceCollide().radius(function (d) {
+      return d.MaternityLeave
+    }))
+    .on('tick', ticked)
+
+  let bubbles = svg.selectAll('.maternity-bubble')
+    .data(data)
+    .enter().append('g').attr('class', 'maternity-bubble')
+    .append('circle')
+      .attr('r', function (d) {
+        return d.MaternityLeave
+      })
+      .style('fill', function (d,i) {
+        let index = industries.indexOf(d.Industry)
+        return color(index)
+      })
+      .style('stroke', 'black')
+      .style('stroke-width', '0.3')
+
+  let labels = d3.selectAll('.maternity-bubble')
+    .append('g')
+
+  let texts = labels.append('text')
+    .attr('x', function(d) {
+      return d.x
+    })
+    .attr('y', function(d) {
+      return d.y
+    })
+    .attr('dy', -5)
+    .text(function(d) {
+      return d.Company
+    })
+    .style('font-size', '10px')
+    .style('text-anchor', 'middle')
+
+  let numbers = labels.append('text')
+    .attr('x', function(d) {
+      return d.x
+    })
+    .attr('y', function(d) {
+      return d.y
+    })
+    .attr('dy', 10)
+    .style('text-anchor', 'middle')
+    .text(function(d) {
+      return d.MaternityLeave
+    })
+    .style('font-size', '17px')
+
+  function ticked () {
+    bubbles.attr('cx', function (d) {
+      return d.x
+    })
+    .attr('cy', function (d) {
+      return d.y
+    })
+
+    texts.attr('x', function (d) {
+      return d.x
+    })
+    .attr('y', function (d) {
+      return d.y
+    })
+
+    numbers.attr('x', function (d) {
+      return d.x
+    })
+    .attr('y', function (d) {
+      return d.y
+    })
+
+    bubbles.exit().remove()
+    labels.exit().remove()
+    numbers.exit().remove()
+  }
+})
---
+++ line.js
@@ -0,0 +1,130 @@
+let widthLine = 600, heightLine = 600
+let margin = {
+  top: 50,
+  bottom: 50,
+  right: 50,
+  left: 50
+}
+
+// SCALE CREATION
+let categories = ['>= 50','>= 40','>= 30','>= 20','>= 10']
+
+let y = d3.scalePoint()
+  .rangeRound([0, widthLine])
+  .domain(categories)
+
+let lineSvg = d3.select('#maternity-line-chart')
+  .append('svg')
+  .attr('viewBox', -margin.left + ' ' +  -margin.top + ' ' + 800 + ' ' + 800)
+
+let g = lineSvg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+g.append("g")
+    .attr("class", "axis axis--y")
+    .call(d3.axisLeft(y))
+
+function yCenter(value) {
+  if (value < 10) {
+    return 16.5 + y.step() * 5
+  } else if (value >= 10 && value < 20) {
+    return 16.5 + y.step() * 5
+  } else if (value >= 20 && value < 30) {
+    return 16.5 + y.step() * 4
+  } else if (value >= 30 && value < 40) {
+    return 16.5 + y.step() * 3
+  } else if (value >= 40 && value < 50) {
+    return 16.5 + y.step() * 2
+  } else if (value >= 50) {
+    return 16.5 + y.step()
+  }
+}
+
+d3.csv('maternity-data.csv').then(data => {
+  let nodes = data
+
+  // BUBBLE FORCE CREATION
+  let simulation = d3.forceSimulation(data) // creates simulation
+    .force('charge', d3.forceManyBody().strength(10)) // applies attraction or repelling force
+    .force('center', d3.forceCenter(widthLine / 1.5, heightLine  / 1.45)) // pulls points towards a center
+    .force('collision', d3.forceCollide().radius(function (d) {
+      return d.MaternityLeave
+    }))
+    .on('tick', ticked)
+
+  simulation.force('y', d3.forceY().y(function(d) {
+    return yCenter(d.MaternityLeave)
+  }).strength(0.5))
+
+  let bubbles = lineSvg.selectAll('.maternity-bubble')
+    .data(data)
+    .enter().append('g').attr('class', 'maternity-bubble')
+    .append('circle')
+      .attr('r', function (d) {
+        return d.MaternityLeave
+      })
+      .style('fill', function (d,i) {
+        let index = industries.indexOf(d.Industry)
+        return color(index)
+      })
+      .style('stroke', 'black')
+      .style('stroke-width', '0.3')
+
+  let labels = d3.selectAll('.maternity-bubble')
+    .append('g')
+
+  let texts = labels.append('text')
+    .attr('x', function(d) {
+      return d.x
+    })
+    .attr('y', function(d) {
+      return d.y
+    })
+    .attr('dy', -5)
+    .text(function(d) {
+      return d.Company
+    })
+    .style('font-size', '10px')
+    .style('text-anchor', 'middle')
+
+  let numbers = labels.append('text')
+    .attr('x', function(d) {
+      return d.x
+    })
+    .attr('y', function(d) {
+      return d.y
+    })
+    .attr('dy', 10)
+    .style('text-anchor', 'middle')
+    .text(function(d) {
+      return d.MaternityLeave
+    })
+    .style('font-size', '17px')
+
+  function ticked () {
+    bubbles.attr('cx', function (d) {
+      return d.x
+    })
+    .attr('cy', function (d) {
+      return d.y
+    })
+
+    texts.attr('x', function (d) {
+      return d.x
+    })
+    .attr('y', function (d) {
+      return d.y
+    })
+
+    numbers.attr('x', function (d) {
+      return d.x
+    })
+    .attr('y', function (d) {
+      return d.y
+    })
+
+    bubbles.exit().remove()
+    labels.exit().remove()
+    numbers.exit().remove()
+  }
+})
