489a88a1ded876af79387a8572a0ca9d41bf7d65
Mandi Cai <mandi@biobright.org>
2018-09-22 12:00:18 -0400

    Add another section about percentage of companies >= 25 weeks maternity leave

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ d3.json('maternity-leave.json').then(data => {
   chartDisplay(data, industries, color)
   countryComparisonDisplay()
   annotateDisplay()
+  largerPercentageDisplay()
   averageDisplay(data)
 
   function setupSections() {
@@ -54,15 +55,18 @@ d3.json('maternity-leave.json').then(data => {
       showLarger()
     }
     activateFunctions[5] = function () {
-      showSmaller()
+      showLargerPercentage()
     }
     activateFunctions[6] = function () {
-      showAverage()
+      showSmaller()
     }
     activateFunctions[7] = function () {
+      showAverage()
+    }
+    activateFunctions[8] = function () {
     }
 
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < 9; i++) {
       updateFunctions[i] = function () {}
     }
   }
@@ -104,6 +108,79 @@ d3.json('maternity-leave.json').then(data => {
     update(index, progress)
   })
 })
+
+function setupBubbleChart() {
+  d3.select('.average-group').attr('display', 'none')
+  d3.select('.larger-percentage-group').attr('display', 'none')
+  d3.select('.maternity-annotation-group').attr('display', 'none')
+  d3.selectAll('.maternity-bubble').attr('display', 'initial')
+  d3.selectAll('.maternity-circle').attr('r', 0)
+  d3.selectAll('.maternity-circle').transition().duration(750).attr('r', function (d) {
+    return d.MaternityLeave
+  })
+  d3.select('.country-bubble-group').attr('display', 'none')
+  d3.select('.country-annotation-group').attr('display', 'none')
+}
+
+function showCountries() {
+  d3.selectAll('.maternity-bubble').attr('display', 'none')
+  d3.select('.country-bubble-group').attr('display', 'initial')
+  d3.selectAll('.country-circle').attr('r', 0)
+  d3.selectAll('.country-circle').transition().duration(750).attr('r', function (d) {
+    return d.maternityLeave
+  })
+  d3.select('.country-annotation-group').attr('display', 'initial')
+}
+
+function showAnnotations() {
+  d3.selectAll('.maternity-bubble').attr('display', 'initial')
+  d3.select('.maternity-annotation-group').attr('display', 'initial')
+  d3.select('.maternity-annotation-group').attr('opacity', '0')
+  d3.select('.maternity-annotation-group').transition().duration(750).attr('opacity', '1')
+}
+
+function showLarger() {
+  d3.select('.maternity-annotation-group').attr('display', 'none')
+  d3.select('.larger-percentage-group').attr('display', 'none')
+
+  d3.selectAll('.maternity-bubble')
+    .attr('display', function (d) {
+      if (d.MaternityLeave <= 25) {
+        return 'none'
+      } else {
+      }
+    })
+    .transition().duration(750).attr('opacity', function (d) {
+      if (d.MaternityLeave >= 25) {
+        return 1
+      } else {}
+    })
+}
+
+function showSmaller() {
+  d3.selectAll('.maternity-bubble').attr('display', 'initial')
+  d3.select('.average-group').attr('display', 'none')
+  d3.select('.larger-percentage-group').attr('display', 'none')
+
+  d3.selectAll('.maternity-bubble')
+    .attr('display', function (d) {
+      if (d.MaternityLeave >= 15) {
+        return 'none'
+      }
+    })
+    .attr('opacity', function (d) {
+      if (d.MaternityLeave >= 25) {
+        return 1
+      }
+    })
+}
+
+function showAverage() {
+  d3.select('.average-group').attr('display', 'initial')
+  d3.selectAll('.maternity-bubble').attr('display', 'none')
+  d3.select('.average-group').attr('opacity', '0')
+  d3.select('.average-group').transition().duration(750).attr('opacity', '1')
+}
 
 // CHART CREATION
 function chartDisplay(data, industries, color) {
@@ -226,7 +303,7 @@ function averageDisplay(data) {
 
   g.append('text')
     .text('Average number weeks paid maternity leave').attr('dy', 50)
-    .attr('class', 'subtext')
+    .attr('class', 'average-subtext')
 }
 
 function countryComparisonDisplay() {
@@ -235,6 +312,11 @@ function countryComparisonDisplay() {
   let countryLeaves = [{
       country: 'Norway',
       maternityLeave: 35,
+      note: '100% of normal pay'
+    },
+    {
+      country: 'Estonia',
+      maternityLeave: 28,
       note: '100% of normal pay'
     },
     {
@@ -327,61 +409,52 @@ function countryComparisonDisplay() {
   countryAnnotationGroup.transition().duration(750).attr('opacity', 1)
 }
 
-function setupBubbleChart() {
-  d3.select('.average-group').attr('display', 'none')
-  d3.select('.maternity-annotation-group').attr('display', 'none')
-  d3.selectAll('.maternity-bubble').attr('display', 'initial')
-  d3.selectAll('.maternity-circle').attr('r', 0)
-  d3.selectAll('.maternity-circle').transition().duration(750).attr('r', function (d) {
-    return d.MaternityLeave
-  })
-  d3.select('.country-bubble-group').attr('display', 'none')
-  d3.select('.country-annotation-group').attr('display', 'none')
-}
+function largerPercentageDisplay() {
+  let count = 0
+  let total = 0
 
-function showCountries() {
-  d3.selectAll('.maternity-bubble').attr('display', 'none')
-  d3.select('.country-bubble-group').attr('display', 'initial')
-  d3.selectAll('.country-circle').attr('r', 0)
-  d3.selectAll('.country-circle').transition().duration(750).attr('r', function (d) {
-    return d.maternityLeave
-  })
-  d3.select('.country-annotation-group').attr('display', 'initial')
-}
-
-function showAnnotations() {
-  d3.selectAll('.maternity-bubble').attr('display', 'initial')
-  d3.select('.maternity-annotation-group').attr('display', 'initial')
-  d3.select('.maternity-annotation-group').attr('opacity', '0')
-  d3.select('.maternity-annotation-group').transition().duration(750).attr('opacity', '1')
-}
-
-function showLarger() {
-  d3.select('.maternity-annotation-group').attr('display', 'none')
-
-  d3.selectAll('.maternity-bubble').attr('display', function (d) {
-    if (d.MaternityLeave <= 30) {
-      return 'none'
+  d3.selectAll('.maternity-bubble').attr('opacity', function (d) {
+    total++
+    if (d.MaternityLeave >= 25) {
+      count++
     }
   })
+
+  let percentage = Math.round(count / total * 100 * 10) / 10
+
+  let g = svg.append('g')
+    .attr('class', 'larger-percentage-group')
+
+  let groupX = width / 2 - (g.node().getBBox().width / 2)
+
+  g.attr('transform', 'translate(' + groupX + ',' + height / 2 + ')')
+
+  g.append('text')
+    .text(percentage.toString() + '%')
+    .attr('class', 'larger-percentage')
+
+  g.append('text')
+    .text('Percentage offering 25 weeks or more paid maternity leave').attr('dy', 50)
+    .attr('class', 'larger-percentage-subtext')
 }
 
-function showSmaller() {
-  d3.selectAll('.maternity-bubble').attr('display', 'initial')
-  d3.select('.average-group').attr('display', 'none')
+function showLargerPercentage() {
+  d3.select('.larger-percentage-group').attr('display', 'initial')
+  d3.select('.larger-percentage-group').attr('opacity', '0')
+  d3.select('.larger-percentage-group').transition().duration(750).attr('opacity', '1')
 
-  d3.selectAll('.maternity-bubble').attr('display', function (d) {
-    if (d.MaternityLeave >= 15) {
-      return 'none'
+  d3.selectAll('.maternity-bubble')
+    .attr('display', function (d) {
+      if (d.MaternityLeave <= 25) {
+        return 'none'
+      } else {}
+    })
+
+  d3.selectAll('.maternity-bubble').attr('opacity', function (d) {
+    if (d.MaternityLeave >= 25) {
+      return 0.5
     }
   })
-}
-
-function showAverage() {
-  d3.select('.average-group').attr('display', 'initial')
-  d3.selectAll('.maternity-bubble').attr('display', 'none')
-  d3.select('.average-group').attr('opacity', '0')
-  d3.select('.average-group').transition().duration(750).attr('opacity', '1')
 }
 
 function annotateDisplay() {
